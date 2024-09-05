bf27bd8e71196b400368f6a58e56e8d4feed3668
diego <correo.contacto.diego.@gmail.com>
2024-09-05 15:15:39 -0600

    Añadiendo texto principal

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -12,9 +12,33 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        leading: Icon(Icons.menu, color: Colors.grey[800],),
+        backgroundColor: Colors.purple,
+        leading: Padding(
+          padding: const EdgeInsets.only(left:24.0),
+          child: IconButton(icon: Icon(Icons.menu, color: Colors.grey[1200], size: 36), onPressed: () { print('No sé programar'); },),
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right:24.0),
+            child: IconButton(icon: Icon(Icons.person, color: Colors.grey[800],size: 36), onPressed: () { print('otro'); },),
+          )
+        ],
       ),
+      body: const Column(children: [
+        //texto
+        Padding(
+          padding: EdgeInsets.all(24.0),
+          child: Row(
+            children: [
+              Text('I Want to ', style: TextStyle(fontSize: 32)),
+              Text('Eat', style: TextStyle(fontSize: 32,fontWeight: FontWeight.bold,decoration: TextDecoration.underline)),
+            ],
+          ),
+        )
+        //Tap_bar
+
+        //Tap_bar_view
+      ],),
     );
   }
 }
